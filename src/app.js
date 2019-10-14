8410b6c5c70da0d7b7f7ae60a11e327f39c8271d
noa.kotkovski <noah.ciel@gmail.com>
2019-10-14 22:01:42 +0300

    Add about text

--- src/app.js
+++ src/app.js
@@ -20,14 +20,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.render('index',{
         title: 'Weather App',
-        name: 'Andrew Mead'
+        name: 'Noa'
     })
 })
 
 app.get('/about',(req,res)=>{
     res.render('about',{
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Noa'
     })
 })
 
@@ -35,7 +35,7 @@ app.get('/help',(req,res)=>{
     res.render('help',{
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Noa'
     })
 })
 /*
@@ -89,7 +89,7 @@ app.get("/products",(req,res)=>{
 app.get('/help/*',(req,res)=>{
     res.render('404',{
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Noa',
         errorMessage: 'help article not found'
     })
 })
@@ -97,7 +97,7 @@ app.get('/help/*',(req,res)=>{
 app.get('*',(req,res)=>{
     res.render('404',{
         title: '404',
-        name: 'Andrew mead',
+        name: 'Noa',
         errorMessage: 'Page not found'
 
     })
